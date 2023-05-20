db609c03b38c15ee8fe9f1a24970e76c29304c75
shadi-daher <125395742+shadi-daher@users.noreply.github.com>
2023-05-20 15:13:13 +0300

    Add files via upload

---
+++ pet-project-card-main/pet-project-card/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { FaCoffee } from 'react-icons/fa';
+
+function App() {
+
+  return <>
+    <button className="btn btn-primary">Click Here</button>
+    <FaCoffee className="mx-5 fs-1"/>
+  </>;
+}
+
+export default App;
